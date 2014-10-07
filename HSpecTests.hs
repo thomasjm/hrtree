2b1895818d0b079d43058521b99022e5f7dd9b39
Tom McLaughlin <tomm@dropbox.com>
2014-10-06 22:13:33 -0700

    Add some simple search tests

--- HSpecTests.hs
+++ HSpecTests.hs
@@ -1,15 +1,40 @@
 module Main where
 
-import HRTree (rectanglesIntersect, Rect(Rect))
+import HRTree
 import Test.Hspec
 
 main = hspec $ do
-         describe "Rectangle intersections" $ do
-                 it "should identify when two rectangles don't intersect" $
-                   rectanglesIntersect (Rect 0 0 2 2) (Rect 3 3 4 4) `shouldBe` False
-                 it "should identify when two rectangles do intersect" $ do
-                   rectanglesIntersect (Rect 0 0 2 2) (Rect 1 1 3 3) `shouldBe` True
-                   rectanglesIntersect (Rect 0 0 2 2) (Rect 0 0 1 1) `shouldBe` True
-                   rectanglesIntersect (Rect 0 0 2 2) (Rect 0 1 1 2) `shouldBe` True
-                 it "should work when one rectangle is a sliver" $ do
-                   rectanglesIntersect (Rect 0 0 2 2) (Rect 0 1 1 1) `shouldBe` True
+  intersectionTest
+  searchTest
+
+intersectionTest =  describe "Rectangle intersections" $ do
+  it "should identify when two rectangles don't intersect" $
+    rectanglesIntersect (Rect 0 0 2 2) (Rect 3 3 4 4) `shouldBe` False
+  it "should identify when two rectangles do intersect" $ do
+    rectanglesIntersect (Rect 0 0 2 2) (Rect 1 1 3 3) `shouldBe` True
+    rectanglesIntersect (Rect 0 0 2 2) (Rect 0 0 1 1) `shouldBe` True
+    rectanglesIntersect (Rect 0 0 2 2) (Rect 0 1 1 2) `shouldBe` True
+  it "should work when one rectangle is a sliver" $ do
+    rectanglesIntersect (Rect 0 0 2 2) (Rect 0 1 1 1) `shouldBe` True
+
+searchTest = describe "Search" $ do
+  it "should work on a single leaf node" $
+    search (Rect 3 3 5 5) (LeafNode [
+                              (IDRect (Rect 0 0 10 10) "rect1"),
+                              (IDRect (Rect 0 0 1 1) "rect2"),
+                              (IDRect (Rect 2 2 7 7) "rect3")
+                              ])
+    `shouldBe` ["rect1", "rect3"]
+
+  it "should work on a single interior node with a single leaf node" $
+    search (Rect 3 3 5 5) (IntNode 42 (Rect 0 0 10 10)
+                           [(LeafNode [
+                               (IDRect (Rect 0 0 10 10) "rect1"),
+                               (IDRect (Rect 0 0 1 1) "rect2"),
+                               (IDRect (Rect 2 2 7 7) "rect3")
+                           ])])
+    `shouldBe` ["rect1", "rect3"]
+
+-- Local Variables:
+-- compile-command: "cabal test"
+-- End:
