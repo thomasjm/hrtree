659741a670af94632681ad78de8a86bc324a3275
Tom McLaughlin <tomm@dropbox.com>
2014-10-06 22:13:46 -0700

    Zipper works now! Going to try parameterizing

--- HRTree.hs
+++ HRTree.hs
@@ -1,6 +1,7 @@
 module HRTree where
 
 import Data.Bits
+import Data.List (foldl')
 
 -- Understand Z-order, Hilbert curve, and Gray-code curve
 
@@ -25,19 +26,70 @@ hilbertDistance d (x,y)
 
 
 
-data Rect = Rect Integer Integer Integer Integer deriving (Show)
-data Obj = Obj String
-data LHV = LHV Integer
+type LHV = Integer
+type ID = String
+data Rect = Rect Integer Integer Integer Integer deriving (Show, Eq)
+data IDRect = IDRect { getRect :: Rect, getId :: ID } deriving (Show, Eq)
+data Node = IntNode LHV Rect [Node] | LeafNode [IDRect] deriving (Show, Eq)
 
+
+---- Utility functions ----
 rectanglesIntersect :: Rect -> Rect -> Bool
 rectanglesIntersect (Rect xl1 yl1 xh1 yh1) (Rect xl2 yl2 xh2 yh2) =
     not ((xh1 < xl2 || xl1 > xh2) || (yh1 < yl2 || yl1 > yh2))
 
+isFull (LeafNode idRects) = length idRects == cl
+isFull (IntNode _ _ children) = length children == cn
 
-data Node = IntNode Obj LHV Rect [Node] | LeafNode [Rect]
+lastN :: Int -> [a] -> [a]
+lastN n xs = foldl' (const .drop 1) xs (drop n xs)
+
+---------------------------
+
 
 type RTree = Node
 
-search :: Node -> Rect -> [Rect]
-search (LeafNode xs) w = filter (rectanglesIntersect w) xs
-search (IntNode _ lhv rect leaves) w = undefined
+search :: Rect -> Node -> [ID]
+search w (LeafNode idRects) = map getId $ filter ((rectanglesIntersect w) . getRect) idRects
+search w (IntNode _ _ children) = concat $ map (search w) children
+
+a & b = b a
+
+data Cxt = Top | Descend (LHV, Rect) [Node] Cxt [Node] deriving (Show, Eq)
+type Loc = (Node, Cxt)
+
+left :: Loc -> Loc
+left (n, Descend dat leftNodes parentCxt rightNodes) = (last leftNodes, Descend dat (init leftNodes) parentCxt (n : rightNodes))
+left (_, Top) = error "Tried to left on top"
+
+right :: Loc -> Loc
+right (n, Descend dat leftNodes parentCxt rightNodes) = (head rightNodes, Descend dat (leftNodes ++ [n]) parentCxt (tail rightNodes))
+right (_, Top) = error "Tried to right on top"
+
+down :: Int -> Loc -> Loc
+down n (IntNode lhv rect children, cxt) = (children !! n, Descend (lhv, rect) (take n children) cxt (lastN (length children - n - 1) children))
+down _ (LeafNode _, _) = error "Tried to down on leaf node"
+
+up :: Loc -> Loc
+up (_, Top) = error "Tried to go up on top"
+up (n, Descend (lhv, rect) leftNodes cxt rightNodes) = (IntNode lhv rect (leftNodes ++ (n : rightNodes)), cxt)
+
+treeTest = (IntNode 42 (Rect 0 0 10 10) [(LeafNode [
+                                             (IDRect (Rect 0 0 10 10) "A1"),
+                                             (IDRect (Rect 0 0 1 1) "A2"),
+                                             (IDRect (Rect 2 2 7 7) "A3")
+                                             ]),
+                                         (LeafNode [
+                                             (IDRect (Rect 0 0 10 10) "B1"),
+                                             (IDRect (Rect 0 0 1 1) "B2")
+                                             ]),
+                                         (LeafNode [
+                                             (IDRect (Rect 0 0 10 10) "C1"),
+                                             (IDRect (Rect 0 0 1 1) "C2"),
+                                             (IDRect (Rect 2 2 7 7) "C3")
+                                             ])])
+
+topLoc = (treeTest, Top)
+
+
+-- chooseLeaf :: Rect -> Node -> Lens Node
