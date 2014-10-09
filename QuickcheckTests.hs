96068f938730460a61d2b685e4754f5676a4db75
Tom McLaughlin <thomasjm@stanford.edu>
2014-10-08 22:13:55 -0700

    Added arbitrary instances

--- QuickcheckTests.hs
+++ QuickcheckTests.hs
@@ -7,16 +7,24 @@ import Control.Monad
 
 main = putStrLn "asdf"
 
--- tree :: Node
--- tree = sized tree'
+instance Arbitrary Node where
+  arbitrary = do
+    x <- tree
+    return x
 
--- tree' :: Node
--- tree' 0 = liftM2 LeafNode arbitrary [arbitrary]
--- tree' n | n>0 =
--- 	oneof [liftM2 LeafNode  arbitrary [arbitrary],
--- 	       liftM3 IntNode arbitrary arbitrary [arbitrary]]
---   where subtree = tree' (n `div` 2)
+tree :: Gen Node
+tree = sized tree'
 
+tree' :: Int -> Gen Node
+tree' 0 = liftM2 LeafNode arbitrary arbitrary
+tree' n | n>0 =
+	oneof [liftM2 LeafNode arbitrary arbitrary,
+	       liftM3 IntNode arbitrary arbitrary arbitrary]
+  where subtree = tree' (n `div` 2)
+
+
+-- instance Arbitrary Char where
+--   arbitrary = elements (['A'..'Z'] ++ ['a' .. 'z'])
 
 instance Arbitrary Rect where
   arbitrary = do
@@ -25,3 +33,9 @@ instance Arbitrary Rect where
     dx <- arbitrary
     dy <- arbitrary
     return $ Rect xl yl (xl + abs dx) (yl + abs dy)
+
+instance Arbitrary IDRect where
+  arbitrary = do
+    r <- arbitrary
+    id <- elements (['A'..'Z'] ++ ['a' .. 'z'])
+    return $ IDRect r [id]
